4906548c09c1dcb874e816a9f48f4ac717adb18e
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-22 21:16:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,12 +11,11 @@ end
 
  def consolidate_cart(cart)
   consolidated_cart = []
-  
-  i = 0     
-  while i < cart.length do
+
     cart.each do |item_hash|
       
       if e = find_item_by_name_in_collection(item_hash[:item], consolidated_cart)
+        
         e[:count] += 1 
       
       else
@@ -25,9 +24,7 @@ end
       end
      
     end
-  i += 1 
-  end
-
+binding.pry 
   consolidated_cart
 end
 
