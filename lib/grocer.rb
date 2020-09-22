3a0324dcb895cd4efd49d1911315e312541956f2
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-22 21:06:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,9 +16,9 @@ end
   while i < cart.length do
     cart.each do |item_hash|
       
-      if exist = find_item_by_name_in_collection(item_hash[:item], consolidated_cart)
+      if e = find_item_by_name_in_collection(item_hash[:item], consolidated_cart)
         
-        exist[:count] += 1 
+        e[:count] += 1 
       
       else
         cart[i][:count] = 1
